80cfaf7acb689c39552c7496a1de3978f7faf175
polsalees <itssupercled@gmail.com>
2024-09-19 12:04:17 +0200

    Change the window size and add 2 buttons, only the exit button works

--- views/main_window.py
+++ views/main_window.py
@@ -1,11 +1,30 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Car Tracker App")
-        self.setGeometry(100, 100, 600, 400)  # Set the window size
+        self.setWindowTitle("Take Car!")
+        self.setGeometry(100, 100, 1500, 900)  # Set the window size
+
+        # Create central widget and layout
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
+
+        # Add buttons
+        self.button1 = QPushButton("Enter", self)
+        self.button1.clicked.connect(self.option1_clicked)
+        self.layout.addWidget(self.button1)
+
+        self.exit_action = QPushButton("Exit", self)
+        self.exit_action.clicked.connect(self.close)
+        self.layout.addWidget(self.exit_action)
+
+
+    def option1_clicked(self):
+        print("Option 1 clicked")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
